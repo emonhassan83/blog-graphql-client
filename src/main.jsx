57106465faa6a5a871fd89daf3ccf37c77db26b4
Emon <emonhasan7650@gmail.com>
2024-06-24 21:00:07 +0600

    apllo client graphql project setup done

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,39 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
+      <ApolloProvider client={client}>
         <RouterProvider router={router} />
+      </ApolloProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
